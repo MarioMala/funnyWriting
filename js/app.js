577ba11a752d8cab3cf5bcf18f2fb32776f8c1eb
Mariusz Małasiewicz <malasiewicz.mariusz@gmail.com>
2023-12-10 18:57:48 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+/** @format */
+const text = document.querySelector('h1');
+const color = document.querySelector('#range-color');
+const size = document.querySelector('#range-size');
+const labelBox = document.querySelector('.label-box');
+const fontSize = document.querySelector('.current-size-value');
+const fontFamily = document.querySelector('.current-fontface-value');
+const selector = document.getElementById('font-selector');
+
+const FONT_FAMILY_MAPPING = {
+	'--fontOswald': 'Oswald',
+	'--fontTeko': 'Teko',
+	'--fontUbuntu': 'Ubuntu',
+};
+
+const changeColor = e => {
+	let r = e.target.value;
+	let g = +e.target.value + Math.ceil(Math.random() * 254);
+	let b = +e.target.value + Math.ceil(Math.random() * 254);
+
+	text.style.color = `rgb(${r}, ${g}, ${b})`;
+};
+
+const changeSize = e => {
+	text.style.fontSize = +e.target.value + 'rem';
+
+	fontSize.textContent = `Bieżaca wielkość czcionki to ${e.target.value} rem`;
+};
+
+const changeFontFamily = e => {
+	let fontFace = e.target.value;
+	text.style.fontFamily = `var(${fontFace})`;
+	
+	fontFamily.textContent = `Bieżacy krój czcionki to ${FONT_FAMILY_MAPPING[fontFace]}`;
+};
+
+color.addEventListener('change', changeColor);
+
+size.addEventListener('input', changeSize);
+
+selector.addEventListener('change', changeFontFamily);
